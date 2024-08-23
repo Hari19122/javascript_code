9a1a665f44c11bf8ba31c42eb9ad567ab2f72099
Harish19122 <hari61bala45@gmail.com>
2024-08-23 11:30:15 +0530

    sample code

---
+++ index.js
@@ -0,0 +1,2 @@
+var hello = document.getElementById("lorem")
+console.log(hello);
---
+++ sam.js
@@ -0,0 +1,320 @@
+// const price = 50
+// const product = "IPHONE 15"
+// const tax = 10
+// console.log(product)
+// var total = price + tax
+// console.log(total);
+
+
+// const Fruitname = "Orange"
+// const Count = 20
+// const price = 65
+// const Total = Count*price
+// console.log(Fruitname);
+// console.log(Total);
+
+
+
+// var naveen = 500
+// var hari = naveen++   //increment
+// console.log(hari);
+// console.log(naveen);
+
+
+// var naveen = 500
+// var hari = --naveen  //pre decrement
+// console.log(hari);
+// console.log(naveen);
+
+
+
+                                    //Datatypes
+
+// console.log(typeof(10));
+// var a= 10
+// console.log(typeof(a));
+// var b="Be a Man"
+// console.log(typeof(b));
+// var hari = true
+// console.log(typeof(hari));
+// var a = null
+// console.log(a);
+
+
+                                //Javascript Function
+
+// function Hari() 
+// {
+//     console.log("He is an Rider");    
+// }
+// Hari()
+
+// function naveen()
+// {
+//     console.log("He loves Sneha");
+// }
+// naveen()
+
+// var a = 10
+// var b = 20
+// c=a+b
+// function add1()
+// {
+//     console.log(a+b);
+// }
+
+// function add2()
+// {
+//     console.log("This is Naveen");
+// }
+// add1()
+// add2()
+
+
+
+// var FActor = "Ajith"
+// var FPlayer = "Dhoni"
+//  var FMovie = "Interseller"
+// var task = { 
+//     favourite_Actor : "Ajith",
+//     favourite_Player : "Dhoni",
+//     favourite_Movie : "Interseller"
+
+// };
+
+// function favourite() 
+// {
+//     console.log(task);
+// }
+
+// favourite()
+
+// function Fav()
+// {
+//     console.log("favourite Actor :" + FActor);
+//     console.log("favourite Player :" + FPlayer);
+//     console.log("favourite Movie :" + FMovie);
+// }
+
+// Fav()
+
+                    //Function Parameters
+
+// function add(a,b)   //parameters passed in a, b in call functions
+// {
+//     console.log(a+b);
+// }
+
+// add(50,50)
+
+// function add(a,b) {
+//     console.log(a+b);
+// }
+
+// add("hari","Keerthysuresh")
+
+
+// function hello(length,breadth) {
+//     console.log("length : " + length);
+//     console.log("breadth : " + breadth);
+//     console.log(length*breadth);
+// }
+
+// hello(50,20)
+
+                        //Parameters values givens in length & breadth
+
+
+// var length = 50
+// var breadth = 80
+
+// function area(l,b) {
+//     var a = l*b
+//     console.log("Area is the :" +a);
+// }
+
+// area(50,80)
+
+
+// var product = [{
+//     Name : "Bajaj",
+//     Model : "Pulsar",
+//     CC : 150,
+//     Engine : "DTS",
+//     Price : 125000
+// },
+// {
+//     Name : "Yamaha",
+//     Model : "R15",
+//     CC : 150,
+//     Engine : "VVA",
+//     Price : 200000
+// },
+// {
+//     Name : "Triumph",
+//     CC : 675,
+//     Engine : "Iltaian Made",
+//     Price : 1200000
+// },
+// {
+//     Name : "RE",
+//     CC : 350,
+//     Engine : "IDK",
+//     Price : 175000
+// },
+// ]
+
+// function Bikename()
+// {
+//     return "product"
+// }
+
+// var z = Bikename()
+// console.log(z);
+
+// function add(a,b)
+// {
+//     return(a*b)
+// }
+// var total = add(80,10)
+// console.log(total);
+
+
+
+            //If Else
+// let a = 1
+// let b = 4
+
+// if (a/b) {
+//     console.log("Great Job");
+// }
+
+// else{
+//     console.log("Finish Your Homework before Playing");
+// }
+
+// var Naveen = true
+
+
+// if (Naveen) {
+//     console.log("He is an Artist");
+// } else {
+//     console.log("He is Not an Artist");
+// }
+
+
+// var season = "Summer"
+
+// if (season == "Spring") {
+//     console.log("Enjoy the blooming Flower");
+// }
+
+// if (season == "Summer") {
+//     console.log("Have Fun In the Sun");
+// }
+
+// if (season == "Autumn") {
+//     console.log("Admire the Colorful Leaves");
+// }
+
+// var Score = 201
+
+// if (Score < 200) {
+//     console.log("Keep it up");
+// }
+// else if (Score < 200 && Score > 150) {
+//     console.log("Excel Powder");
+// }
+// else if (Score >= 200) {
+//     console.log("Good Team");
+// }
+
+
+// const sneha= 21
+// if (sneha < 22) {
+//     console.log(" odi poiruu");
+// } 
+// else {
+//     console.log("vanthudu");
+// }
+
+
+
+// let Meghna = 13
+// if (Meghna < 12) {
+//     console.log("Met in class");
+// }
+// else{
+//     console.log("Met in terrace");
+// }
+
+//for looping
+
+// for (index = 1; index <= 10; index++) {
+    
+//     console.log(index);
+// }/
+
+// for (index = 10; index > 0; index--) {
+    
+//     console.log(index);
+// }
+
+
+// for (index = 1; index <= 10; index++) {
+//      let a = index * 2
+//         console.log(index + "x2=" + a);
+//     }
+
+
+// if(false){
+//     console.log("Its Naveen");
+// }
+// else{
+//     console.log("Its not naveen");
+    
+// }
+
+
+
+// var a = 25
+
+// if(a>50)
+// {
+//     console.log("Its true");
+// }
+
+// else if(a<50 && a>10)
+// {
+//     console.log("its false");
+// }
+// else if(a>=50)
+// {
+//     console.log("Undefined");
+    
+// }
+
+
+// for( i = 1; i <= 10; i = i+1)
+// {
+//     console.log("john");
+    
+// }
+
+// console.log("john");
+
+
+// var a = naveen
+
+// for( i = 1; i<=10; i=i+1)
+// {
+//     console.log(i);
+    
+// }
+
+
+// for(i = 10; i >= 1; i = --i)
+// {
+//     console.log(i);
+    
+// }
